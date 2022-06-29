76d4ca0fb122ff38dbf3758489532d9e1dcc5370
Natalia Netesova <natalia-info@bk.ru>
2022-06-29 23:28:00 +0300

    1 task sloved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1 (Семинар) найдите квадрат числа
+
+Console.Write("Enter your number:");
+int number = int.Parse(Console.ReadLine());
+int result = number*number;
+Console.Write("Square of your number: ");
+Console.WriteLine(result);
+
+// Задача №2 (Семинар) 
