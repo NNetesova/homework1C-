ecbfb15705437e1f92a91a6d99263b2bdc0f914d
Natalia Netesova <natalia-info@bk.ru>
2022-06-29 23:45:24 +0300

    2 repeat

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// Задача №1 (Семинар) найдите квадрат числа
 
-Console.Write("Enter your number:");
-int number = int.Parse(Console.ReadLine()); //перевод значения в число
-int result = number*number;
-Console.Write("Square of your number: ");
-Console.WriteLine(result);
+//Console.Write("Enter your number:");
+//int number = int.Parse(Console.ReadLine()); //перевод значения в число
+//int result = number*number;
+//Console.Write("Square of your number: ");
+//Console.WriteLine(result);
 
 // Задача №2 (Семинар) По двум заданным числам проверить является ли первое квадратом второго 
 
