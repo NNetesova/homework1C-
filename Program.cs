6848b09da62cf2c43e0303a7aaec205c7598db39
Natalia Netesova <natalia-info@bk.ru>
2022-06-29 23:42:44 +0300

    2 task sloved

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 ﻿// Задача №1 (Семинар) найдите квадрат числа
 
 Console.Write("Enter your number:");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()); //перевод значения в число
 int result = number*number;
 Console.Write("Square of your number: ");
 Console.WriteLine(result);
 
-// Задача №2 (Семинар) 
+// Задача №2 (Семинар) По двум заданным числам проверить является ли первое квадратом второго 
+
+Console.Write("ВВедите число numberA: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("ВВедите число numberB: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA == numberB*numberB)
+{
+    Console.Write("Да, первое число является квадратом второго");
+}
+else
+{
+    Console.Write("Нет, первое число не является квадратом второго");
+}
