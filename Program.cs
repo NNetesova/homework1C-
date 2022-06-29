7f19536c6a9fdd91522cdc8c200ed841c3655290
Natalia Netesova <natalia-info@bk.ru>
2022-06-30 00:12:22 +0300

    3 task sloved

--- Program.cs
+++ Program.cs
@@ -8,16 +8,36 @@
 
 // Задача №2 (Семинар) По двум заданным числам проверить является ли первое квадратом второго 
 
-Console.Write("ВВедите число numberA: ");
+//Console.Write("ВВедите число numberA: ");
+//int numberA = int.Parse(Console.ReadLine());
+//Console.Write("ВВедите число numberB: ");
+//int numberB = int.Parse(Console.ReadLine());
+//
+//if (numberA == numberB*numberB)
+//{
+//    Console.Write("Да, первое число является квадратом второго");
+//}
+//else
+//{
+//    Console.Write("Нет, первое число не является квадратом второго");
+//}
+
+// Задача №3 (Семинар) Найдите наибольшее и наименьшее число из двух
+
+Console.Write("ВВедите число A: ");
 int numberA = int.Parse(Console.ReadLine());
-Console.Write("ВВедите число numberB: ");
+Console.Write("ВВедите число B: ");
 int numberB = int.Parse(Console.ReadLine());
 
-if (numberA == numberB*numberB)
+if (numberA>numberB)
 {
-    Console.Write("Да, первое число является квадратом второго");
+  Console.Write("Число А наибольшее, число В наименьшее");  
+}
+else if (numberA<numberB)
+{
+    Console.Write("Число А наименьшее, число В наибольшее");
 }
 else
 {
-    Console.Write("Нет, первое число не является квадратом второго");
+    Console.Write("Число А равно числу В");
 }
