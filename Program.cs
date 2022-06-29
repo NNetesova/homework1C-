704f9a87f3a3488bb296899442d306e1ef0bc8e5
Natalia Netesova <natalia-info@bk.ru>
2022-06-30 00:21:39 +0300

    4 task sloved

--- Program.cs
+++ Program.cs
@@ -24,20 +24,34 @@
 
 // Задача №3 (Семинар) Найдите наибольшее и наименьшее число из двух
 
-Console.Write("ВВедите число A: ");
-int numberA = int.Parse(Console.ReadLine());
-Console.Write("ВВедите число B: ");
-int numberB = int.Parse(Console.ReadLine());
+//Console.Write("ВВедите число A: ");
+//int numberA = int.Parse(Console.ReadLine());
+//Console.Write("ВВедите число B: ");
+//int numberB = int.Parse(Console.ReadLine());
+//
+//if (numberA>numberB)
+//{
+//  Console.Write("Число А наибольшее, число В наименьшее");  
+//}
+//else if (numberA<numberB)
+//{
+//    Console.Write("Число А наименьшее, число В наибольшее");
+//}
+//else
+//{
+//    Console.Write("Число А равно числу В");
+//}
 
-if (numberA>numberB)
+// Задача №4 (Семинар) Проверить является ли число четным или нечетным
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+if (A%2==0)
 {
-  Console.Write("Число А наибольшее, число В наименьшее");  
-}
-else if (numberA<numberB)
-{
-    Console.Write("Число А наименьшее, число В наибольшее");
+    Console.Write("Число является четным");
 }
 else
 {
-    Console.Write("Число А равно числу В");
+    Console.Write("Число является нечетным");
 }
